69f8c9f59633c143ab00f0f3d39a3352e4294089
Marcos Fraga <marcosjsfraga@gmail.com>
2020-11-29 23:44:52 -0300

    Project and Task CRUD

--- app/Controllers/Http/TaskController.js
+++ app/Controllers/Http/TaskController.js
@@ -1,92 +1,81 @@
 'use strict'
 
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Task = use('App/Models/Task')
 
-/**
- * Resourceful controller for interacting with tasks
- */
 class TaskController {
     /**
-   * Show a list of all tasks.
-   * GET tasks
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-    async index ({ request, response, view }) {
+     * Show a list of all tasks.
+     * GET tasks
+     */
+    async index ({ params }) {
+        const tasks = await Task.query()
+            .where('project_id', params.projects_id)
+            .with('user')
+            .fetch()
+
+        return tasks
     }
 
     /**
-   * Render a form to be used for creating a new task.
-   * GET tasks/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-    async create ({ request, response, view }) {
+     * Create/save a new task.
+     * POST tasks
+     */
+    async store ({ params, request }) {
+        const data = request.only([
+            'user_id',
+            'title',
+            'description',
+            'due_date',
+            'file_id'
+        ])
+
+        const task = await Task.create({ ...data, project_id: params.projects_id })
+
+        return task
     }
 
     /**
-   * Create/save a new task.
-   * POST tasks
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-    async store ({ request, response }) {
+     * Display a single task.
+     * GET tasks/:id
+     */
+    async show ({ params }) {
+        const task = await Task.findOrFail(params.id)
+
+        await task.load('user')
+
+        return task
     }
 
     /**
-   * Display a single task.
-   * GET tasks/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-    async show ({ params, request, response, view }) {
-    }
-
-    /**
-   * Render a form to update an existing task.
-   * GET tasks/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-    async edit ({ params, request, response, view }) {
-    }
-
-    /**
-   * Update task details.
-   * PUT or PATCH tasks/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
+     * Update task details.
+     * PUT or PATCH tasks/:id
+     */
     async update ({ params, request, response }) {
+        const data = request.only([
+            'user_id',
+            'title',
+            'description',
+            'due_date',
+            'file_id'
+        ])
+
+        const task = await Task.findOrFail(params.id)
+
+        task.merge(data)
+
+        await task.save()
+
+        return task
     }
 
     /**
-   * Delete a task with id.
-   * DELETE tasks/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-    async destroy ({ params, request, response }) {
+     * Delete a task with id.
+     * DELETE tasks/:id
+     */
+    async destroy ({ params }) {
+        const task = await Task.findOrFail(params.id)
+
+        task.delete()
     }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -15,8 +15,14 @@ Route.put('forgotpassword', 'ForgotPasswordController.update')
 // Files
 Route.get('files/:id', 'FileController.show')
 
+// Routes that need authentication
 Route.group(() => {
+    // Files
     Route.post('files', 'FileController.store')
+
     // Project (All routes in one declaration)
     Route.resource('projects', 'ProjectController').apiOnly()
+
+    // Tasks (All routes with project_id dependecy)
+    Route.resource('projects.tasks', 'TaskController').apiOnly()
 }).middleware(['auth'])
